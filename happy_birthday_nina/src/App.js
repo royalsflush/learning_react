eb79f186e29c96f792f5b5f6fe0ab335563b263a
Thomas N Tomei <thomas@royalsflush.com>
2024-10-18 21:44:57 +0100

    Added one more key

--- happy_birthday_nina/src/App.js
+++ happy_birthday_nina/src/App.js
@@ -1,4 +1,34 @@
 import './App.css';
+import useSound from 'use-sound';
+
+function playHappyBirthday() {
+  const fsharp3 = document.getElementById('fsharp3');
+  const a4 = document.getElementById('a4');
+
+  fsharp3.click();
+
+  setTimeout(function() { fsharp3.click() 
+    setTimeout(function() {
+      a4.click()
+        setTimeout(function() {
+          fsharp3.click()
+        }.bind(this), 700);
+    }.bind(this), 400);
+  }.bind(this), 400);
+}
+
+const Key = (props) => {
+  const [play] = useSound('/marimba_sounds/'+ props.key_name + '.wav');
+  const clazz = 'keys key-'+props.key_name;
+
+  return (
+    <div
+      id={props.key_name}
+      className={clazz}
+      onClick={play}>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -8,15 +38,21 @@ function App() {
           className="marimba"
           src="./marimba.png"
           alt="marimba"
-          style={{"pointer-events": "all"}}
         />
-        <div className="keys key-one"></div>
-        <div className="keys key-two"></div>
-        <div className="keys key-three"></div>
-        <div className="keys key-four"></div>
-        <div className="keys key-five"></div>
+
+        <Key key_name="esharp2" />
+        <Key key_name="f2" />
+        <Key key_name="a2" />
+        <Key key_name="b3" />
+        <Key key_name="c3" />
+        <Key key_name="e3" />
+        <Key key_name="fsharp3" />
+        <Key key_name="g3" />
+        <Key key_name="a4" />
+        <Key key_name="d4" />
+        <Key key_name="esharp4" />
         </div>
-      <button>Play happy birthday!</button>
+      <button onClick={playHappyBirthday}>Play happy birthday!</button>
     </div>
   );
 }
