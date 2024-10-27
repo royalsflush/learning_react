d3f883fb57fe02da63a4eae37d2b04919131d4c7
Thomas N Tomei <thomas@royalsflush.com>
2024-10-27 23:27:25 +0000

    Small refactoring, created a module for rendering the marimba

--- happy_birthday_nina/src/App.js
+++ happy_birthday_nina/src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import useSound from 'use-sound';
-
-
+import Marimba from './Marimba.js';
 
 function playHappyBirthdayOnce() {
   const fsharp3 = document.getElementById('fsharp3');
@@ -65,51 +63,10 @@ function playHappyBirthday() {
   playHappyBirthdayOnce();
 }
 
-const Key = (props) => {
-  const [play] = useSound('/marimba_sounds/'+ props.key_name + '.mp3');
-  const clazz = 'keys key-'+props.key_name;
-
-  return (
-    <div
-      id={props.key_name}
-      className={clazz}
-      onClick={play}>
-    </div>
-  );
-}
-
 function App() {
   return (
     <div className="App">
-        <div className="marimba-container">
-        <img
-          className="marimba"
-          src="./marimba.png"
-          alt="marimba"
-        />
-
-        <Key key_name="esharp2" />
-        <Key key_name="f2" />
-        <Key key_name="a2" />
-        <Key key_name="b3" />
-        <Key key_name="c3" />
-        <Key key_name="e3" />
-        <Key key_name="fsharp3" />
-        <Key key_name="g3" />
-        <Key key_name="a4" />
-        <Key key_name="d4" />
-        <Key key_name="esharp4" />
-        <Key key_name="f4" />
-        <Key key_name="g4" />
-        <Key key_name="b5" />
-        <Key key_name="c5" />
-        <Key key_name="e5" />
-        <Key key_name="fsharp5" />
-        <Key key_name="a6" />
-        <Key key_name="d6" />
-        <Key key_name="esharp6" />
-        <Key key_name="f6" />
-        </div>
+      <Marimba />
       <button
         className="happy-button"
         onClick={playHappyBirthday}>Play happy birthday!</button>
---
+++ happy_birthday_nina/src/Marimba.js
@@ -0,0 +1,50 @@
+import useSound from 'use-sound';
+
+const Key = (props) => {
+  const [play] = useSound('/marimba_sounds/'+ props.key_name + '.mp3');
+  const clazz = 'keys key-'+props.key_name;
+
+  return (
+    <div
+      id={props.key_name}
+      className={clazz}
+      onClick={play}>
+    </div>
+  );
+}
+
+function Marimba() {
+  return (
+    <div className="marimba-container">
+        <img
+          className="marimba"
+          src="./marimba.png"
+          alt="marimba"
+        />
+
+        <Key key_name="esharp2" />
+        <Key key_name="f2" />
+        <Key key_name="a2" />
+        <Key key_name="b3" />
+        <Key key_name="c3" />
+        <Key key_name="e3" />
+        <Key key_name="fsharp3" />
+        <Key key_name="g3" />
+        <Key key_name="a4" />
+        <Key key_name="d4" />
+        <Key key_name="esharp4" />
+        <Key key_name="f4" />
+        <Key key_name="g4" />
+        <Key key_name="b5" />
+        <Key key_name="c5" />
+        <Key key_name="e5" />
+        <Key key_name="fsharp5" />
+        <Key key_name="a6" />
+        <Key key_name="d6" />
+        <Key key_name="esharp6" />
+        <Key key_name="f6" />
+    </div>
+  );
+}
+
+export default Marimba;
