bfadcaf46547014c74ce8117aa85b790c1142dfc
Thomas N Tomei <thomas@royalsflush.com>
2024-10-28 00:33:43 +0000

    Successfully replaced on click for classes, which makes me happy

--- happy_birthday_nina/src/Marimba.js
+++ happy_birthday_nina/src/Marimba.js
@@ -1,11 +1,15 @@
 import useSound from 'use-sound';
-
+import { useState } from 'react'; 
 
 const Key = (props) => {
   const [play, { sound }] = useSound(
     '/marimba_sounds/'+ props.key_name + '.mp3',
     { volume: 1 },
   );
+  const initialState = 'keys key-'+props.key_name;
+  const activeState = initialState + ' key-pressed';
+  const [style, setStyle] = useState(initialState);
+
   function pressKey() {
     // Playing the same sound twice makes it louder, which is needed because
     // this is already the maximum volume for the API.
@@ -13,16 +17,17 @@ const Key = (props) => {
     sound.play();
 
     // Need to add key-pressed class.
+    setStyle(activeState);
   }
   function releaseKey() {
     // Need to remove key-pressed class.
+    setStyle(initialState);
   }
-  const clazz = 'keys key-'+props.key_name;
 
   return (
     <div
       id={props.key_name}
-      className={clazz}
+      className={style}
       onMouseDown={pressKey}
       onMouseUp={releaseKey}>
     </div>
