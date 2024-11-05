d21aa4972b3f6e5282cecee1200caa90ad1965ea
Thomas N Tomei <thomas@royalsflush.com>
2024-11-05 01:51:29 +0000

    Did the initial board 10x10 dynamically. Board is still static and not clickable yet

--- minesweeper/src/App.js
+++ minesweeper/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Board() {
+  const board_size = 10;
+  const board = Array(board_size);
+  
+  for (let i=0; i<board_size; i++) {
+    board[i] = Array(board_size);
+
+    for (let j=0; j<board_size; j++)
+      board[i][j] = '_';
+  }
+
+  return (
+    <table>
+      <tr>
+        <th colspan={board_size}>Minesweeper</th>
+      </tr>
+      {board.map(row => {
+        return (
+          <tr>
+          {row.map(cell => {
+            return <td>{cell}</td>
+          })}
+          </tr>
+        );
+      })}
+    </table>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board />          
     </div>
   );
 }
