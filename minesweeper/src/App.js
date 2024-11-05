ac1add34f4bf152efb79c7137e04e2c492e54228
Thomas N Tomei <thomas@royalsflush.com>
2024-11-05 02:28:49 +0000

    Did the generation of bombs randomly

--- minesweeper/src/App.js
+++ minesweeper/src/App.js
@@ -2,25 +2,41 @@ import './App.css';
 
 function Board() {
   const board_size = 10;
+  const bomb_count = 30;
   const board = Array(board_size);
   
+  const bomb_pos = [];
+
   for (let i=0; i<board_size; i++) {
     board[i] = Array(board_size);
 
     for (let j=0; j<board_size; j++)
-      board[i][j] = '_';
+      board[i][j] = {
+        content: '_'
+      }
+  }
+
+  while (bomb_pos.length < bomb_count) {
+    let x = Math.floor(Math.random() * board_size * board_size)
+    if (bomb_pos.indexOf(x) == -1) bomb_pos.push(x)
+  }
+
+  for (let i=0; i<bomb_count; i++) {
+    let x = Math.floor(bomb_pos[i]/board_size);
+    let y = bomb_pos[i]%board_size;
+    board[x][y].content = 'B'
   }
 
   return (
     <table>
       <tr>
-        <th colspan={board_size}>Minesweeper</th>
+        <th colSpan={board_size}>Minesweeper</th>
       </tr>
       {board.map(row => {
         return (
           <tr>
           {row.map(cell => {
-            return <td>{cell}</td>
+            return <td>{cell.content}</td>
           })}
           </tr>
         );
