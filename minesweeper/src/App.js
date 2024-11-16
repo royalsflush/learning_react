4449e7b2e6f9be39b8c82e8cb4aa3d0c4fdd3261
Thomas N Tomei <thomas@royalsflush.com>
2024-11-16 18:59:26 +0000

    Implemented the bomb counter, looks like it works good

--- minesweeper/src/App.js
+++ minesweeper/src/App.js
@@ -4,10 +4,35 @@ function calculateBombPosition(board_size, bomb_count) {
   const bomb_pos = [];
   while (bomb_pos.length < bomb_count) {
     let x = Math.floor(Math.random() * board_size * board_size)
-    if (bomb_pos.indexOf(x) == -1) bomb_pos.push(x)
+    if (bomb_pos.indexOf(x) === -1) bomb_pos.push(x)
   }
 
   return bomb_pos;
+}
+
+function countAdjacentBombs(board, i, j) {
+  const pos = [[-1, -1], [-1, 0], [-1, 1], [0, -1],
+        [0, 1], [1, 0], [1, 1], [1, -1]];
+
+  // This should be an assertion, but I think that's it's bad form to
+  // fail so disruptively during a live application
+  if (board.length <= i || board[i].length <= j) {
+    console.log("Invalid coordinates passed to countAdjacentBombs");
+    return 0;
+  }
+
+  let cnt=0;
+  for (let k=0; k<pos.length; k++) {
+    const ni = i+pos[k][0];
+    const nj = j+pos[k][1];
+
+    if (ni<0 || ni>=board.length) continue;
+    if (nj<0 || nj>=board[i].length) continue;
+
+    if (board[ni][nj].content === 'B') cnt++;
+  }
+
+  return cnt;
 }
 
 function initialiseBoard(board_size, bomb_pos) {
@@ -29,6 +54,13 @@ function initialiseBoard(board_size, bomb_pos) {
     let x = Math.floor(bomb_pos[i]/board_size);
     let y = bomb_pos[i]%board_size;
     board[x][y].content = 'B'
+  }
+
+  for (let i=0; i<board_size; i++) {
+    for (let j=0; j<board_size; j++) {
+      if (board[i][j].content === 'B') continue;
+      board[i][j].content = countAdjacentBombs(board, i, j);
+    }
   }
 
   return board;
